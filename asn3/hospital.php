35eafa3467d14f7e1ad4ad85642da91c230556ba
Aman Tawakley <atawakl@uwo.ca>
2022-11-27 12:38:59 -0500

    hospital page

--- asn3/hospital.php
+++ asn3/hospital.php
@@ -4,18 +4,24 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hospital Database</title>
+  <title>Hospital Database - Doctors</title>
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+  <!-- Database connect -->
+  <?php
+    include 'connectdb.php';
+    session_start();
+  ?>
   <!-- Header on all pages -->
   <div class="header">
-    <h2 class="text-center my-auto" href="http://cs3319.gaul.csd.uwo.ca/vm269/a3panda/">Hospital Database</h2>
+    <a class="h2 text-center my-auto" href="http://cs3319.gaul.csd.uwo.ca/vm269/a3panda/">Hospital Database</a>
   </div>
-  <a class="btn btn-primary" href="./doctors.php" role="button">Doctors</a>
-  <a class="btn btn-primary" href="./looksafter.php" role="button">Doctor-Patient</a>
-  <a class="btn btn-primary" href="./hospital.php" role="button">Hospital</a>
+
+  <div class="footer mt-3">
+    <p class="text-center my-auto">48's head hurts from all the PHP 😫</p>
+  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
